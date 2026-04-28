291b370524b6cb2910e5e8be6967284bf8ec7a39
Thomas GUERY <originaltompouce@gmail.com>
2026-04-28 15:07:26 +0200

    bump version

--- src/anapconfig.js
+++ src/anapconfig.js
@@ -5,10 +5,9 @@ var ANAP_CONFIG = {
     REFRESH_TIMER_BIG: 30000,
     MEDIUM_WORLD_THRESHOLD: 100,
     BIG_WORLD_THRESHOLD: 300,
-    APP_VERSION: '0.2',
+    APP_VERSION: '0.2.1',
     SETTINGS_VERSION: '2',
     OFFLINE: false,
-    URL_WEBSITE: 'https://tomagueri.fr/anaptracker',
 };
 
 export default ANAP_CONFIG;
